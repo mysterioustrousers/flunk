7ea6a544e9fb669fab6bfee4787a9d234bcf7ad5
Adam Kirk <atomkirk@gmail.com>
2013-01-26 14:42:12 -0700

    cleaning up testing

--- test/factories/bananas.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :banana do
-    size 1
-    weight 1
-    color "MyString"
-  end
-end
--- test/unit/helpers/bananas_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class BananasHelperTest < ActionView::TestCase
-end
